f8ff3e5c6b3df6196253e8b7165df93f5d56637d
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2017-12-16 11:37:48 +0100

    First commit

---
+++ Source/LibNoiseExample/LibNoiseExampleGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LibNoiseExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIBNOISEEXAMPLE_API ALibNoiseExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
